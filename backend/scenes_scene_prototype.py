80c6849a2d184630468061cc1144e9663249e738
Matthias <matthias.plock@bam.de>
2017-09-14 13:51:20 +0200

    tests and simplification

--- backend/scenes_scene_prototype.py
+++ backend/scenes_scene_prototype.py
@@ -17,6 +17,9 @@ class _ScenePrototype:
     On initialization a unique identifier is generated and assigned to the
     scene.
 
+    When a dataset is added to a scene the unique identifier of the dataset is
+    returned, otherwise None. Likewise for deleting a dataset.
+
     Args:
      data_dir (os.PathLike): A path pointing to the directory containing our
       simulation data.
@@ -61,12 +64,129 @@ class _ScenePrototype:
          None: No parameters.
 
         Returns:
-         str: The name (scene_hash) of the scene.
+         str: The name (hash) of the scene.
 
         """
         return self._scene_name
 
-    def _add_one_dataset(self, dataset_path):
+    # def _add_one_dataset(self, dataset_path):
+    #     """
+    #     Add one dataset.
+
+    #     Verify that the given object path contains simulation data that we
+    #     can add. Do this by checking for a /fo or /frb subpath.
+
+    #     Args:
+    #      object_path (os.Pathlike): The path to the dataset we want to add
+
+    #     Returns:
+    #      dict: The metadata of the dataset we added.
+
+    #     Raises:
+    #      TypeError: If `dataset_path` is not `os.Pathlike`.
+    #      ValueError: If `dataset_path` does not exist.
+    #      ValueError: If `dataset_path` is not a directory.
+    #      ValueError: If there is no 'fo' and/or 'frb' sub directory in
+    #       `dataset_path.`
+
+    #     """
+    #     if not isinstance(dataset_path, os.PathLike):
+    #         raise TypeError(
+    #             'dataset_path is {}, expected os.PathLike'.format(
+    #                 type(dataset_path).__name__))
+
+    #     if not dataset_path.exists():
+    #         raise ValueError('path \'{}\' does not exist'.format(
+    #             dataset_path))
+
+    #     if not dataset_path.is_dir():
+    #         raise ValueError('dataset_path must point to a directory')
+
+    #     # Casts this to os.pathlike
+    #     fo_dir = dataset_path / 'fo'
+    #     frb_dir = dataset_path / 'frb'
+
+    #     # Raise an exception in case there are no subfolders called 'fo' or
+    #     # 'frb'
+    #     if not (fo_dir.exists() or frb_dir.exists()):
+    #         raise ValueError(
+    #             '{} contains neither \'fo\' nor \'frb\''.format(
+    #                 dataset_path))
+
+    #     # Create and append a new dataset
+    #     new_dataset = _DatasetPrototype(dataset_path=dataset_path)
+    #     dataset_meta = new_dataset.meta()
+    #     dataset_hash = dataset_meta['datasetHash']
+    #     self._dataset_list[dataset_hash] = new_dataset
+
+    #     return dataset_meta
+
+    # def add_datasets(
+    #         self,
+    #         dataset_list
+    # ):
+    #     """
+    #     Add one or multiple dataset(s) to the scene.
+
+    #     Args:
+    #      dataset_list (list (of str)): The relative path to the object
+    #       root, relative to `data_dir`.
+
+    #     Raises:
+    #      TypeError: If an entry in `dataset_list` is not `str`.
+    #      TypeError: If ``type(dataset_list)`` is not `list`.
+    #      ValueError: If ``len(dataset_list)`` is `0`.
+
+    #     Notes:
+    #      See FIXME in code.
+
+    #     """
+    #     if not isinstance(dataset_list, list):
+    #         raise TypeError(
+    #             'dataset_list is {}, expected list'.format(
+    #                 type(dataset_list).__name__))
+
+    #     if len(dataset_list) == 0:
+    #         raise ValueError('dataset_list is empty')
+
+    #     # Encode the scene hash into the return_dict
+    #     return_dict = {
+    #         'href': '/scenes/{}'.format(self._scene_name),
+    #         'addDatasetsSuccess': []
+    #     }
+
+    #     # Add each object
+    #     for one_dataset in dataset_list:
+    #         try:
+    #             if not isinstance(one_dataset, str):
+    #                 raise TypeError(
+    #                     'one_dataset is {}, expected str'.format(
+    #                         type(one_dataset).__name__))
+
+    #             # Cast to os.PathLike
+    #             one_dataset_path = self._data_dir / one_dataset
+    #             dataset_meta = self._add_one_dataset(one_dataset_path)
+    #             return_dict['addDatasetsSuccess'].append(dataset_meta)
+
+    #         # Catch everything that could have gone wrong and just report that
+    #         # the dataset could not be added. NOTE: This also catches the case
+    #         # that an entry in the list was not a string, so we might run in to
+    #         # trouble? But it came from a list, so it can also go back into a
+    #         # list I guess... Maybe FIXME.
+    #         except (TypeError, ValueError):
+    #             try:
+    #                 return_dict['addDatasetsFail'].append(one_dataset)
+    #             except KeyError:
+    #                 return_dict['addDatasetsFail'] = []
+    #                 return_dict['addDatasetsFail'].append(one_dataset)
+
+    #     # If we have nothing to return..
+    #     if len(return_dict['addDatasetsSuccess']) == 0:
+    #         return None
+    #     else:
+    #         return return_dict
+
+    def add_dataset(self, dataset_path):
         """
         Add one dataset.
 
@@ -116,76 +236,11 @@ class _ScenePrototype:
         dataset_hash = dataset_meta['datasetHash']
         self._dataset_list[dataset_hash] = new_dataset
 
-        return dataset_meta
-
-    def add_datasets(
-            self,
-            dataset_list
-    ):
-        """
-        Add one or multiple dataset(s) to the scene.
-
-        Args:
-         dataset_list (list (of str)): The relative path to the object
-          root, relative to `data_dir`.
-
-        Raises:
-         TypeError: If an entry in `dataset_list` is not `str`.
-         TypeError: If ``type(dataset_list)`` is not `list`.
-         ValueError: If ``len(dataset_list)`` is `0`.
-
-        Notes:
-         See FIXME in code.
-
-        """
-        if not isinstance(dataset_list, list):
-            raise TypeError(
-                'dataset_list is {}, expected list'.format(
-                    type(dataset_list).__name__))
-
-        if len(dataset_list) == 0:
-            raise ValueError('dataset_list is empty')
-
-        # Encode the scene hash into the return_dict
-        return_dict = {
-            'href': '/scenes/{}'.format(self._scene_name),
-            'addDatasetsSuccess': []
-        }
-
-        # Add each object
-        for one_dataset in dataset_list:
-            try:
-                if not isinstance(one_dataset, str):
-                    raise TypeError(
-                        'one_dataset is {}, expected str'.format(
-                            type(one_dataset).__name__))
-
-                # Cast to os.PathLike
-                one_dataset_path = self._data_dir / one_dataset
-                dataset_meta = self._add_one_dataset(one_dataset_path)
-                return_dict['addDatasetsSuccess'].append(dataset_meta)
-
-            # Catch everything that could have gone wrong and just report that
-            # the dataset could not be added. NOTE: This also catches the case
-            # that an entry in the list was not a string, so we might run in to
-            # trouble? But it came from a list, so it can also go back into a
-            # list I guess... Maybe FIXME.
-            except (TypeError, ValueError):
-                try:
-                    return_dict['addDatasetsFail'].append(one_dataset)
-                except KeyError:
-                    return_dict['addDatasetsFail'] = []
-                    return_dict['addDatasetsFail'].append(one_dataset)
-
-        # If we have nothing to return..
-        if len(return_dict['addDatasetsSuccess']) == 0:
-            return None
-        else:
-            return return_dict
+        return dataset_hash
 
     def list_datasets(self):
         """
-        Returns a list of all the objects in this scene.
+        Return a list of all the objects in this scene.
 
         Create a sorted view (list) of the keys of the dict
         `self._dataset_list`.
@@ -197,13 +252,15 @@ class _ScenePrototype:
         list_of_datasets = sorted(self._dataset_list.keys())
         return list_of_datasets
 
-    def delete_dataset(self, dataset_hash):
+    def dataset(self, dataset_hash):
         """
-        Remove one dataset from the scene.
+        Return a dataset object for working with.
+
+        Args:
+         dataset_hash: The unique identifier for the dataset we want to access.
 
         Returns:
-         bool: True if the dataset could be removed, False if the dataset
-         could not be removed (e.g. did not exist in the first place).
+         _DatasetPrototype: The dataset that we want to access.
 
         Raises:
          TypeError: If ``type(dataset_hash)`` is not `str`.
@@ -214,10 +271,34 @@ class _ScenePrototype:
                 type(dataset_hash).__name__))
 
         try:
-            self._dataset_list.pop(dataset_hash)
-            return True
+            return self._dataset_list[dataset_hash]
         except KeyError:
-            return False
+            raise ValueError('dataset_hash does not fit any dataset in scene')
+
+    def delete_dataset(self, dataset_hash):
+        """
+        Remove one dataset from the scene.
+
+        Args:
+         dataset_hash: The unique identifier for the dataset we want to delete.
+
+        Returns:
+         str: The hash of the dataset that was removed from the scene.
+
+        Raises:
+         TypeError: If ``type(dataset_hash)`` is not `str`.
+         ValueError: If `dataset_hash` does not fit any dataset in the scene.
+
+        """
+        if not isinstance(dataset_hash, str):
+            raise TypeError('dataset_hash is {}, expected str'.format(
+                type(dataset_hash).__name__))
+
+        try:
+            self._dataset_list.pop(dataset_hash)
+            return dataset_hash
+        except KeyError:
+            raise ValueError('dataset_hash does not fit any dataset in scene')
 
     # DELETING MULTIPLE DATASETS IS NOT NECESSARY BY DEFINITION OF THE API.
     # def delete_datasets(self, dataset_hash_list):
--- backend/tests/test_scenes_scene_prototype.py
+++ backend/tests/test_scenes_scene_prototype.py
@@ -11,6 +11,7 @@ import sys
 import os
 sys.path.append(os.path.join('..', '..'))  # Append the program root dir
 from backend.scenes_scene_prototype import _ScenePrototype
+from backend.scenes_dataset_prototype import _DatasetPrototype
 
 
 class Test_scenes_scene_prototype(unittest.TestCase):
@@ -40,104 +41,106 @@ class Test_scenes_scene_prototype(unittest.TestCase):
         with self.assertRaises(ValueError):
             _ScenePrototype(non_existing_dataset)
 
-    def test_scene_name_is_sha1(self):
-        """
-        Test if the format is correct, that is 40 hex chars.
-
-        Regex this: '^[0-9a-f]{40}$'
-
-        ^        Start Of String Anchor
-        [0-9a-f] Any of the following characters: 0123456789abcdef
-        {40}     Repeated 40 times
-        $        End Of String Anchor
-
-        Adapted from https://stackoverflow.com/questions/2982059/testing-if-string-is-sha1-in-php
-
-        """
+        # Test if the format is correct, that is 40 hex chars.
+        #
+        # Regex this: '^[0-9a-f]{40}$'
+        #
+        # ^        Start Of String Anchor
+        # [0-9a-f] Any of the following characters: 0123456789abcdef
+        # {40}     Repeated 40 times
+        # $        End Of String Anchor
+        #
+        # Adapted from https://stackoverflow.com/questions/2982059/testing-if-string-is-sha1-in-php
+        #
         scene_name = _ScenePrototype(self.data_dir_path).name()
         self.assertRegex(scene_name, '^[0-9a-f]{40}$')
 
-    def test__add_one_dataset(self):
+    def test_add_dataset(self):
         """
-        Test what happens when we want to add one dataset.
+        Test what happens when we want to add a dataset.
 
         """
+        scene = _ScenePrototype(self.data_dir_path)
+
+        datasets = scene.list_datasets()
+        self.assertTrue(len(datasets) == 0)
+
         just_fo_string = 'just_fo'
         dataset_path = self.data_dir_path / just_fo_string
 
-        # Correct path
-        dataset_meta = _ScenePrototype(self.data_dir_path)._add_one_dataset(dataset_path)
-        self.assertIsInstance(dataset_meta, dict)
-        self.assertIn('datasetAlias', dataset_meta)
-        self.assertIn('datasetHash', dataset_meta)
-        self.assertIn('datasetHref', dataset_meta)
-        self.assertIn('datasetName', dataset_meta)
+        # Returns dataset hash
+        dataset_hash = scene.add_dataset(dataset_path)
+        self.assertIsInstance(dataset_hash, str)
+        self.assertRegex(dataset_hash, '^[0-9a-f]{40}$')
+
+        datasets = scene.list_datasets()
+        self.assertTrue(len(datasets) == 1)
 
         # Wrong argument type
         with self.assertRaises(TypeError):
-            _ScenePrototype(self.data_dir_path)._add_one_dataset('{}/{}.'.format(
+            _ScenePrototype(self.data_dir_path).add_dataset('{}/{}.'.format(
                 self.data_dir_string, just_fo_string))
 
         # Non exising path
         non_existing_path = dataset_path / 'does_not_exist'
         with self.assertRaises(ValueError):
-            _ScenePrototype(self.data_dir_path)._add_one_dataset(non_existing_path)
+            _ScenePrototype(self.data_dir_path).add_dataset(non_existing_path)
 
         # Points to file instead of dir
         not_a_dir = dataset_path / 'not_a_directory'
         with self.assertRaises(ValueError):
-            _ScenePrototype(self.data_dir_path)._add_one_dataset(not_a_dir)
+            _ScenePrototype(self.data_dir_path).add_dataset(not_a_dir)
 
-    def test_add_datasets(self):
-        """
-        Test adding one or more objects.
+    # def test_add_datasets(self):
+    #     """
+    #     Test adding one or more objects.
 
-        """
-        # Should not contain addDatasetsFail
-        dataset_list_success = ['just_fo', 'another_just_fo']
-        success_dict = _ScenePrototype(self.data_dir_path).add_datasets(dataset_list_success)
-        self.assertIsInstance(success_dict, dict)
-        self.assertIn('href', success_dict)
-        self.assertIn('addDatasetsSuccess', success_dict)
-        self.assertNotIn('addDatasetsFail', success_dict)
+    #     """
+    #     # Should not contain addDatasetsFail
+    #     dataset_list_success = ['just_fo', 'another_just_fo']
+    #     success_dict = _ScenePrototype(self.data_dir_path).add_datasets(dataset_list_success)
+    #     self.assertIsInstance(success_dict, dict)
+    #     self.assertIn('href', success_dict)
+    #     self.assertIn('addDatasetsSuccess', success_dict)
+    #     self.assertNotIn('addDatasetsFail', success_dict)
 
-        # Appending identical datasets, should not contain addDatasetsFail
-        dataset_list_success = ['just_fo', 'another_just_fo', 'just_fo', 'another_just_fo']
-        success_dict = _ScenePrototype(self.data_dir_path).add_datasets(dataset_list_success)
-        self.assertIsInstance(success_dict, dict)
-        self.assertIn('href', success_dict)
-        self.assertIn('addDatasetsSuccess', success_dict)
-        self.assertNotIn('addDatasetsFail', success_dict)
+    #     # Appending identical datasets, should not contain addDatasetsFail
+    #     dataset_list_success = ['just_fo', 'another_just_fo', 'just_fo', 'another_just_fo']
+    #     success_dict = _ScenePrototype(self.data_dir_path).add_datasets(dataset_list_success)
+    #     self.assertIsInstance(success_dict, dict)
+    #     self.assertIn('href', success_dict)
+    #     self.assertIn('addDatasetsSuccess', success_dict)
+    #     self.assertNotIn('addDatasetsFail', success_dict)
 
-        # Should contain both addDatasetsSuccess and addDatasetsFail
-        dataset_list_partial = ['no_such_thing', 'another_just_fo']
-        partial_dict = _ScenePrototype(self.data_dir_path).add_datasets(dataset_list_partial)
-        self.assertIsInstance(partial_dict, dict)
-        self.assertIn('href', partial_dict)
-        self.assertIn('addDatasetsSuccess', partial_dict)
-        self.assertIn('addDatasetsFail', partial_dict)
+    #     # Should contain both addDatasetsSuccess and addDatasetsFail
+    #     dataset_list_partial = ['no_such_thing', 'another_just_fo']
+    #     partial_dict = _ScenePrototype(self.data_dir_path).add_datasets(dataset_list_partial)
+    #     self.assertIsInstance(partial_dict, dict)
+    #     self.assertIn('href', partial_dict)
+    #     self.assertIn('addDatasetsSuccess', partial_dict)
+    #     self.assertIn('addDatasetsFail', partial_dict)
 
-        # Should contain both addDatasetsSuccess and addDatasetsFail
-        dataset_list_partial_not_just_string = ['no_such_thing', 'another_just_fo', 123]
-        partial_dict_not_just_string = _ScenePrototype(self.data_dir_path).add_datasets(dataset_list_partial_not_just_string)
-        self.assertIsInstance(partial_dict_not_just_string, dict)
-        self.assertIn('href', partial_dict_not_just_string)
-        self.assertIn('addDatasetsSuccess', partial_dict_not_just_string)
-        self.assertIn('addDatasetsFail', partial_dict_not_just_string)
+    #     # Should contain both addDatasetsSuccess and addDatasetsFail
+    #     dataset_list_partial_not_just_string = ['no_such_thing', 'another_just_fo', 123]
+    #     partial_dict_not_just_string = _ScenePrototype(self.data_dir_path).add_datasets(dataset_list_partial_not_just_string)
+    #     self.assertIsInstance(partial_dict_not_just_string, dict)
+    #     self.assertIn('href', partial_dict_not_just_string)
+    #     self.assertIn('addDatasetsSuccess', partial_dict_not_just_string)
+    #     self.assertIn('addDatasetsFail', partial_dict_not_just_string)
 
-        # Just return None
-        dataset_list_fail = ['just_fo_', 'another_just_fo_']
-        fail_return = _ScenePrototype(self.data_dir_path).add_datasets(dataset_list_fail)
-        self.assertIsNone(fail_return)
+    #     # Just return None
+    #     dataset_list_fail = ['just_fo_', 'another_just_fo_']
+    #     fail_return = _ScenePrototype(self.data_dir_path).add_datasets(dataset_list_fail)
+    #     self.assertIsNone(fail_return)
 
-        # Empty list should fail
-        empty_list = []
-        with self.assertRaises(ValueError):
-            _ScenePrototype(self.data_dir_path).add_datasets(empty_list)
+    #     # Empty list should fail
+    #     empty_list = []
+    #     with self.assertRaises(ValueError):
+    #         _ScenePrototype(self.data_dir_path).add_datasets(empty_list)
 
-        just_a_string = 'just_fo'
-        with self.assertRaises(TypeError):
-            _ScenePrototype(self.data_dir_path).add_datasets(just_a_string)
+    #     just_a_string = 'just_fo'
+    #     with self.assertRaises(TypeError):
+    #         _ScenePrototype(self.data_dir_path).add_datasets(just_a_string)
 
     def test_list_datasets(self):
         """
@@ -145,111 +148,104 @@ class Test_scenes_scene_prototype(unittest.TestCase):
 
         """
         # Construct the scene
-        dataset_list_one = ['just_fo']
-        dataset_list_two = ['just_fo', 'another_just_fo']
-
-        # Append one element to it
         scene = _ScenePrototype(self.data_dir_path)
-        scene.add_datasets(dataset_list_one)
-        datasets = scene.list_datasets()
-        self.assertTrue(len(datasets) == 1)
 
-        # Append two more elements, now there are three in it
-        scene.add_datasets(dataset_list_two)
         datasets = scene.list_datasets()
-        self.assertTrue(len(datasets) == 3)
 
-        # Each is a sha1 sum
-        for entry in datasets:
-            self.assertRegex(entry, '^[0-9a-f]{40}$')
+        self.assertIsInstance(datasets, list)
+        self.assertTrue(len(datasets) == 0)
+
+        # Append two valid datasets
+        scene.add_dataset(self.data_dir_path / 'just_fo')
+        scene.add_dataset(self.data_dir_path / 'another_just_fo')
+
+        datasets = scene.list_datasets()
+        for dataset in datasets:
+            self.assertIsInstance(dataset, str)
+            self.assertRegex(dataset, '^[0-9a-f]{40}$')
+        self.assertTrue(len(datasets) == 2)
 
     def test_delete_dataset(self):
         """
         Test removing one dataset.
 
         """
-        # First construct the scene
-        dataset_list = ['just_fo', 'just_fo', 'another_just_fo']
+        # Construct the scene
         scene = _ScenePrototype(self.data_dir_path)
-        scene.add_datasets(dataset_list)
+
+        # Append three valid datasets
+        scene.add_dataset(self.data_dir_path / 'just_fo')
+        scene.add_dataset(self.data_dir_path / 'just_fo')
+        scene.add_dataset(self.data_dir_path / 'another_just_fo')
+
         cmp_datasets = scene.list_datasets()
+        self.assertTrue(len(cmp_datasets) == 3)
 
         # Remove the 0th element
         dataset_to_delete = cmp_datasets[0]
         cmp_datasets.remove(dataset_to_delete)
 
         # Delete this element from the scene
-        scene.delete_dataset(dataset_to_delete)
+        deleted_hash = scene.delete_dataset(dataset_to_delete)
+        self.assertRegex(deleted_hash, '^[0-9a-f]{40}$')
         res_dataset = scene.list_datasets()
-
         self.assertTrue(cmp_datasets == res_dataset)
 
-        # List raises TypeError
-        dataset_list = ['just_fo', 'just_fo', 'another_just_fo']
-        scene = _ScenePrototype(self.data_dir_path)
-        scene.add_datasets(dataset_list)
+        # Do we have two elements left?
         cmp_datasets = scene.list_datasets()
+        self.assertTrue(len(cmp_datasets) == 2)
 
-        dataset_to_delete_list = [cmp_datasets[0]]
+        # Successful delete returns dataset_hash that was deleted
+        dataset_to_delete = cmp_datasets[0]
+        deleted_dataset_hash = scene.delete_dataset(dataset_to_delete)
+        self.assertIsInstance(deleted_dataset_hash, str)
+        self.assertTrue(dataset_to_delete == deleted_dataset_hash)
 
+        # We have to supply a string as a dataset_hash
+        dataset_to_delete = [cmp_datasets[0]]
         with self.assertRaises(TypeError):
-            scene.delete_dataset(dataset_to_delete_list)
+            scene.delete_dataset(dataset_to_delete)
 
-    # IT IS NOT NECESSARY TO TEST FOR THIS BECAUSE THE API DOES NOT SPECIFY THIS.
-    # def test_delete_datasetss(self):
-    #     """
-    #     Test deleting datasets from the scene.
+        # If we supply the wrong hash we expect a ValueError
+        dataset_to_delete = 'wrong'
+        with self.assertRaises(ValueError):
+            scene.delete_dataset(dataset_to_delete)
 
-    #     """
-    #     # First construct the scene
-    #     dataset_list = ['just_fo', 'just_fo', 'another_just_fo']
-    #     scene_one = _ScenePrototype(self.data_dir_path)
-    #     scene_one.add_datasets(dataset_list)
-    #     cmp_datasets_one = scene_one.list_datasets()
+        # Do we have one element left?
+        cmp_datasets = scene.list_datasets()
+        self.assertFalse(len(cmp_datasets) == 0)
+        self.assertFalse(len(cmp_datasets) == 2)
+        self.assertTrue(len(cmp_datasets) == 1)
 
-    #     self.assertTrue(len(cmp_datasets_one) == 3)
+    def test_dataset(self):
+        """
+        Test the dataset return method.
 
-    #     dataset_to_delete = cmp_datasets_one[0]
-    #     delete_list = [dataset_to_delete]
-    #     scene_one.delete_datasets(delete_list)
-    #     res_datasets_one = scene_one.list_datasets()
+        """
+        # Construct the scene
+        scene = _ScenePrototype(self.data_dir_path)
 
-    #     self.assertFalse(len(res_datasets_one) == 3)
-    #     self.assertTrue(len(res_datasets_one) == 2)
+        # Append three valid datasets
+        scene.add_dataset(self.data_dir_path / 'just_fo')
+        scene.add_dataset(self.data_dir_path / 'another_just_fo')
 
-    #     # Removing two has one left
-    #     dataset_list = ['just_fo', 'just_fo', 'another_just_fo']
-    #     scene_two = _ScenePrototype(self.data_dir_path)
-    #     scene_two.add_datasets(dataset_list)
-    #     cmp_datasets_two = scene_two.list_datasets()
-    #     self.assertTrue(len(cmp_datasets_two) == 3)
-    #     datasets_to_delete_list = cmp_datasets_two[0:2]  # Get the first two
+        cmp_datasets = scene.list_datasets()
+        self.assertTrue(len(cmp_datasets) == 2)
 
-    #     scene_two.delete_datasets(datasets_to_delete_list)
-    #     res_datasets_two = scene_two.list_datasets()
+        # Try and access one dataset, it should return an object
+        target_dataset_hash = cmp_datasets[0]
+        target_dataset = scene.dataset(target_dataset_hash)
+        self.assertIsInstance(target_dataset, _DatasetPrototype)
 
-    #     self.assertFalse(len(res_datasets_two) == 3)
-    #     self.assertTrue(len(res_datasets_two) == 1)
+        # We have to supply a string as a dataset_hash
+        dataset_wrong = [cmp_datasets[0]]
+        with self.assertRaises(TypeError):
+            scene.delete_dataset(dataset_wrong)
 
-    #     # Removing all the datasets should yield an exception to be caught
-    #     dataset_list = ['just_fo', 'just_fo', 'another_just_fo']
-    #     scene_three = _ScenePrototype(self.data_dir_path)
-    #     scene_three.add_datasets(dataset_list)
-    #     cmp_datasets_three = scene_three.list_datasets()
-
-    #     with self.assertRaises(IndexError):
-    #         scene_three.delete_datasets(cmp_datasets_three)
-
-
-    #     # String or smth raises TypeError
-    #     scene_four = _ScenePrototype(self.data_dir_path)
-    #     scene_four.add_datasets(dataset_list)
-    #     cmp_datasets_four = scene_four.list_datasets()
-    #     dataset_to_delete = cmp_datasets_four[0]
-
-    #     with self.assertRaises(TypeError):
-    #         scene_four.delete_datasets(dataset_to_delete)
-
+        # If we supply the wrong hash we expect a ValueError
+        dataset_wrong = 'wrong'
+        with self.assertRaises(ValueError):
+            scene.dataset(dataset_wrong)
 
 if __name__ == '__main__':
     """
